ab65e477a963a89a6197daf45a75ce9166aec8d2
KATHIRVEL L <140801916+LKathirvel@users.noreply.github.com>
2024-03-22 20:03:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle menu 
+
+function showMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.style.right = '0';
+}
+
+function hideMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.style.right = '-200px';
+}
